2ad99a5c4a0a4d623c96f34b1a89e7dcdaf691b3
Ram10000001 <ramsan060602@gmail.com>
2024-05-07 13:42:19 -0600

    RE

--- MAI/MAI/settings.py
+++ MAI/MAI/settings.py
@@ -104,7 +104,7 @@ WSGI_APPLICATION = 'MAI.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         "NAME": "neuronas",
         "USER": "postgres",
         "PASSWORD": "R@ms2002",
