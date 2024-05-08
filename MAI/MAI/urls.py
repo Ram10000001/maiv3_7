4eaeddbfbfb667acdfc294f607de62072ec7ba01
Carlos Luvina <itlac19122004@lcardenas.tecnm.mx>
2024-05-07 22:39:29 -0600

    cambio de MAI.MAI a .

--- MAI/MAI/urls.py
+++ MAI/MAI/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
 
-from MAI.MAI import settings
+from . import settings
 from . import views
 
 urlpatterns = [
