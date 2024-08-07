61525a76b3d7f28e0762f8ccb9e6e75bfdeb52f4
Carlos <italc19122004@lcardenas.tecnm.mx>
2024-08-06 20:10:33 -0600

    Creacion frontend

--- MAI/settings.py
+++ MAI/settings.py
@@ -30,8 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app",
-                 ".now.sh", "mai-634u.onrender.com"]
+ALLOWED_HOSTS = ["127.0.0.1",".now.sh", "mai-634u.onrender.com"]
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
@@ -170,7 +169,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend', 'dist', 'frontend')]
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
